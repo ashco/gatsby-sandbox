e7014f321a55799405b66cbe16e2719cbaa900fd
Ashton <ashchristie@gmail.com>
2018-08-27 11:40:14 -0700

    added in new pages, menu component

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const AboutPage = () => (
+  <div>
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, magnam id.
+      Saepe facilis esse, maiores eum ex at earum pariatur incidunt, vero magni
+      molestiae. Voluptatum officiis iste pariatur rerum? Quaerat.
+    </p>
+  </div>
+)
+
+export default AboutPage
